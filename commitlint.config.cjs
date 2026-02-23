8381a5342b150bd759675b34286aa99acc38b8e5
Laurick Bouquet <laurick.bouquet@tenexa.com>
2026-02-23 13:13:01 +0100

    ci: commit lint rules

---
+++ commitlint.config.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  parserPreset: {
+    parserOpts: {
+      headerPattern: /^(feat|fix|perf|refactor|test|docs|style|ci|build): (.+)$/,
+      headerCorrespondence: ['type', 'subject'],
+    },
+  },
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'perf', 'refactor', 'test', 'docs', 'style', 'ci', 'build'],
+    ],
+    'type-empty': [2, 'never'],
+    'scope-empty': [2, 'always'],
+    'subject-empty': [2, 'never'],
+  },
+};
