bfd26f772078198db531570d8a50ac505f371c12
Nicolas_dmb <gyfeix@gmail.com>
2026-07-01 13:49:25 +0200

    feat : Add aoi_id and aoi_version columns to zones_hex table

--- infra/postgres/init/01_zones_hex.sql
+++ infra/postgres/init/01_zones_hex.sql
@@ -4,6 +4,8 @@ CREATE TABLE zones_hex (
     geom GEOMETRY(POLYGON, 4326) NOT NULL,
     centroid GEOMETRY(POINT, 4326),
     bbox GEOMETRY(POLYGON, 4326),
+    aoi_id TEXT NOT NULL, 
+    aoi_version TEXT NOT NULL,
 
     CONSTRAINT uq_zones_hex_zone_resolution
         UNIQUE (zone_id, resolution)
